0e91516df134e19069e5bf30f598edeb34908c58
kbates <bates78@purdue.edu>
2020-10-12 16:31:08 -0400

    original version

---
+++ src/main/java/io/guthub/kbates23/monopoly/Monopoly.java
@@ -0,0 +1,17 @@
+package io.guthub.kbates23.monopoly;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Monopoly extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
